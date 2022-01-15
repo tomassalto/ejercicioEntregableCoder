1650aeb35429790fe3f2fd8a53ff5d90b8066bbe
tomassalto <tomasagustinsalto@gmail.com>
2022-01-14 23:10:07 -0300

    Update tomasSalto.js
    agregadas las alternativas

--- javascript/tomasSalto.js
+++ javascript/tomasSalto.js
@@ -1,15 +1,25 @@
 let cantSueldo = parseInt(prompt("Ingrese la cantidad de sueldos: "));
 
-sumaSueldo = 0;
-promedio = 0;
 
-for (i = 1; i <= cantSueldo; i++){
+if(cantSueldo >= 0){
 
-   let sueldo = parseInt(prompt("Ingrese el sueldo: "));
-   sumaSueldo = sumaSueldo + sueldo; 
+   sumaSueldo = 0;
+   promedio = 0;
+
+   for (i = 1; i <= cantSueldo; i++){
+ 
+      let sueldo = parseInt(prompt("Ingrese el sueldo "+i+": "));
+      sumaSueldo = sumaSueldo + sueldo; 
+             
+   }
    
+   promedio = sumaSueldo / cantSueldo;
+   
+   console.log(promedio);
+
+}else{
+
+   respuesta = "Error! Ingrese un numero mayor a 0";
+   console.log(respuesta);
 }
 
-promedio = sumaSueldo / cantSueldo;
-
-console.log(promedio);
